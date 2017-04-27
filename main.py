cf77d5b49416c33b05955a66450adda289a342d0
Amos Garner <aagarner.7@gmail.com>
2017-04-27 10:29:16 -0400

    Implement dynamic username and collection type

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import datetime, json, os.path, argparse
 CONST_COLLECTIONS_NAME = 'collections'
 
 def generateArgumentsFromParser():
-    parser = parser = argparse.ArgumentParser(description="Runs PyTriSearch googling utility.")
+    parser = parser = argparse.ArgumentParser(description="Runs the PyInventory utility for creating a collection of items.")
+    parser.add_argument('--user', dest='username', required=True)
+    parser.add_argument('--type', dest='collectionName', required=True)
     return parser.parse_args()
 
 def generateItemsCollection(collectionName, username):
@@ -21,19 +23,15 @@ def generateItemsCollection(collectionName, username):
 def main():
         arguments = generateArgumentsFromParser()
 
-        username = 'agarner'
-        collectionName = 'Items'
+        createCollection(arguments.username, arguments.collectionName)
+        itemCollection = generateItemsCollection(arguments.collectionName, arguments.username)
 
-        createCollection(username,collectionName)
-        itemCollection = generateItemsCollection(collectionName, username)
+        collectionsFilePath = CONST_COLLECTIONS_NAME+'/'+arguments.username+'_'+CONST_COLLECTIONS_NAME+'/'+arguments.username+'_'+arguments.collectionName+'_'+'collection.dat'
 
-        collectionsFilePath = CONST_COLLECTIONS_NAME+'/'+username+'_'+CONST_COLLECTIONS_NAME+'/'+username+'_'+collectionName+'_'+'collection.dat'
         if os.path.isfile(collectionsFilePath):
             collectionFile = open(collectionsFilePath, 'w')
             collectionFile.write(itemCollection.toJSON())
             collectionFile.close()
-
-
 
 if __name__ == '__main__':
     main()
