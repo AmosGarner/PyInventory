7c5bddf805ecbc7844cf3ea9f43e923296f8f1ae
Amos Garner <aagarner.7@gmail.com>
2017-04-27 11:29:09 -0400

    Refactor main to generate collection based on type.

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ CONST_COLLECTIONS_NAME = 'collections'
 def generateArgumentsFromParser():
     parser = parser = argparse.ArgumentParser(description="Runs the PyInventory utility for creating a collection of items.")
     parser.add_argument('--user', dest='username', required=True)
-    parser.add_argument('--type', dest='collectionName', required=True)
+    parser.add_argument('--type', dest='collectionType', required=True)
+    parser.add_argument('--name', dest='collectionName', required=True)
     return parser.parse_args()
 
-def generateItemsCollection(collectionName, username):
+def generateCollection(collectionType, collectionName, username, length):
     items = []
     now = datetime.datetime.now()
-    for i in range(0,10):
-        item = ItemFactory.factory('item', [i, 'item' + str(i), now, now])
-        print(item.name)
-        items.append(item)
+
+    if collectionType.lower() == 'item':
+        for i in range(0,length):
+            item = ItemFactory.factory('item', [i, 'item' + str(i), now, now])
+            items.append(item)
+
     return Collection(collectionName, username, items)
 
 def main():
         arguments = generateArgumentsFromParser()
 
         createCollection(arguments.username, arguments.collectionName)
-        itemCollection = generateItemsCollection(arguments.collectionName, arguments.username)
+        itemCollection = generateCollection('item', arguments.collectionName, arguments.username, 10)
 
         collectionsFilePath = CONST_COLLECTIONS_NAME+'/'+arguments.username+'_'+CONST_COLLECTIONS_NAME+'/'+arguments.username+'_'+arguments.collectionName+'_'+'collection.dat'
 
