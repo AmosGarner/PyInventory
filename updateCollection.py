ed6aed33d5474b63243ce7200a1c293f4b377fbb
Amos Garner <aagarner.7@gmail.com>
2017-04-28 10:44:11 -0400

    [Incremenetal] implementation of data extraction into collection

--- updateCollection.py
+++ updateCollection.py
@@ -1,18 +1,24 @@
 from DataObjects.Collection import Collection
 from ObjectFactories.ItemFactory import ItemFactory
-import os
+import json
 
 def main():
     updateCollection(
         Collection('items', 'agarner', []),
         ItemFactory.factory('item', [0, 'someItem', 'date', 'date']),
-        'collections/agarner_collections/agarner_items_collection.dat'
+        'collections/agarner_collections/agarner_Items_collection.dat'
     )
+
+def getCollection(fileName):
+    collectionFile = open(fileName, 'r')
+    fileData = json.loads(collectionFile.read())
+    itemData = fileData['items']
+    collection = Collection(fileData['username'], fileData['collectionName'], fileData['items'])
+
 
 def updateCollection(collection, item, fileName):
     collection.items.append(item)
-    for item in collection.items:
-        print item.name
+    getCollection(fileName)
 
 if __name__ == '__main__':
     main()
