7dcf2bbf37a37199cc16b1134b2ef10c14ff2d80
Amos Garner <aagarner.7@gmail.com>
2017-04-28 11:02:40 -0400

    [Incremental] implement of json to collection

--- updateCollection.py
+++ updateCollection.py
@@ -4,16 +4,27 @@ import json
 
 def main():
     updateCollection(
-        Collection('items', 'agarner', []),
+        Collection('item','Items', 'agarner', []),
         ItemFactory.factory('item', [0, 'someItem', 'date', 'date']),
-        'collections/agarner_collections/agarner_Items_collection.dat'
+        'collections/agarner_collections/agarner_Item_collection.dat'
     )
 
 def getCollection(fileName):
     collectionFile = open(fileName, 'r')
     fileData = json.loads(collectionFile.read())
     itemData = fileData['items']
-    collection = Collection(fileData['username'], fileData['collectionName'], fileData['items'])
+
+    itemArr = []
+    for value in itemData:
+        item = ItemFactory.factory(
+            fileData['collectionType'],
+            value.values()
+        )
+        itemArr.append(item)
+
+    collection = Collection(fileData['collectionType'], fileData['collectionName'], fileData['username'], itemArr)
+    for item in collection.items:
+        print(item.id)
 
 
 def updateCollection(collection, item, fileName):
