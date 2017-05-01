debb2388a2cc6d58e0ad4b0032d4b8625fc86427
Amos Garner <aagarner.7@gmail.com>
2017-05-01 15:22:52 -0400

    Refactor arguments passable through cli

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ CONST_OBJECT_TYPES = ['item', 'album', 'book', 'movie']
 
 def generateArgumentsFromParser():
     parser = parser = argparse.ArgumentParser(description="Runs the PyInventory utility for creating a collection of items.")
+    parser.add_argument('--action', dest='action', required=True)
     parser.add_argument('--user', dest='username', required=True)
     parser.add_argument('--type', dest='collectionType', required=True)
-    parser.add_argument('--name', dest='collectionName', required=True)
-    parser.add_argument('--length', dest='length', required=True)
-    parser.add_argument('--test', dest='test', required=False)
     return parser.parse_args()
 
 def generateCollection(collectionType, collectionName, username, length):
