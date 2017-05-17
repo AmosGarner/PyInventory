9bbc3f96d385fef3f1d318d8cadb0e2079549579
Amos Garner <aagarner.7@gmail.com>
2017-05-17 17:07:55 -0400

    Remove removeCollection file

--- removeCollection.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def removeCollection(collectionFilePath):
-    try:
-        os.remove(collectionFilePath)
-    except OSError:
-        print("Could not remove the collection at path: " + collectionFilePath)
