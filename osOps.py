370f422ff5be4a9bbd40f5616ea5c758493e9d56
Amos Garner <aagarner.7@gmail.com>
2017-05-17 10:02:48 -0400

    Init osOps file for handeling OS file system operations

---
+++ osOps.py
@@ -0,0 +1,14 @@
+def createFile(directoryPath, fileName):
+    return None
+
+def createDirectory(directoryPath):
+    return None
+
+def getFileContents(filePath):
+    return None
+
+def deleteFile(filePath):
+    return None
+
+def deleteDirectory(directoryPath):
+    return None
