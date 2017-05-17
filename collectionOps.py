b5c2603ec929433ae8f37299bb811a1d7d17647b
Amos Garner <aagarner.7@gmail.com>
2017-05-17 13:51:34 -0400

    Implement get collection functionality

--- collectionOps.py
+++ collectionOps.py
@@ -1,2 +1,32 @@
-def getCollection(filePath):
-    return None
+from DataObjects.Collection import Collection
+import json
+
+def getCollection(fileData):
+    try:
+        fileData = json.loads(fileData)
+        return generateCollectionOnFileData(fileData)
+    except:
+        print('Error: Could not load collection data from file.')
+
+def generateCollectionOnFileData(fileData):
+    collectionType = fileData['collectionType']
+    collectionName = fileData['collectionName']
+    username = fileData['username']
+    itemData = fileData['items']
+
+    itemArr = []
+    for value in itemData:
+        if fileData['collectionType'] == 'item':
+            item = ItemFactory.factory(collectionType,[value['id'], value['name'], value['addedOn'], value['lastEdit']])
+            itemArr.append(item)
+        elif fileData['collectionType'] == 'album':
+            item = ItemFactory.factory(collectionType,[value['id'], value['name'], value['addedOn'], value['lastEdit'], value['artist']])
+            itemArr.append(item)
+        elif fileData['collectionType'] == 'book':
+            item = ItemFactory.factory(collectionType,[value['id'], value['name'], value['addedOn'], value['lastEdit'], value['author']])
+            itemArr.append(item)
+        elif fileData['collectionType'] == 'movie':
+            item = ItemFactory.factory(collectionType,[value['id'], value['name'], value['addedOn'], value['lastEdit'], value['director']])
+            itemArr.append(item)
+
+    return Collection(fileData['username'], fileData['collectionName'], fileData['collectionType'], itemArr)
