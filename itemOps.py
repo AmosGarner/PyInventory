d46d17e32329ae8b5b8df5b9294857e8ecde3a01
Amos Garner <aagarner.7@gmail.com>
2017-05-18 10:44:10 -0400

    Init itemOps file with item ops functionality functions

---
+++ itemOps.py
@@ -0,0 +1,8 @@
+def insertItem(collection, item):
+    return collection
+
+def editItem(collection, itemId, itemData):
+    return collection
+
+def removeItem(collection, itemId):
+    return collection
